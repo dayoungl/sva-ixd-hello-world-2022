bd5849b78372ace2f611782f83a6191e22a32dc0
dayoungl <43307109+dayoungl@users.noreply.github.com>
2020-10-13 22:33:17 -0400

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,60 @@
+
+# Sharon Lee
+# dlee71@sva.edu
+
+
+#Assignment 1
+myPerfumes = ["Philosykos", "Wood Sage & Sea Salt", "Palermo", "Dirty"]
+
+fallPerfume = {
+	"name" : "Philosykos",
+	"brand" : "Diptyque",
+	"perfumer": "Olivia Giacobetti",
+	"type" : ["eau de toilette", "unisex"],
+	"size" : "75ml",
+	"baseNote": ["white cedarwood", "woods", "musk"],
+	"topNote" : ["fig leaf", "fig"],
+	"inspirations" : ["Greece", "sea", "wild figs", "dry wind"]
+}
+
+
+#Assignemnt 2
+coffeBeans = ["Three Africas", "Bella Donovan", "Fishtown"]
+
+coffeeBeans[2] = {
+	"Name" = "Bella Donovan",
+	"brand" = "Blue Bottle",
+	"singleOrigin" = False,
+	"notes" = ["raspberry", "chocolate", "molasses"],
+	"price" = "11",
+	'bafSize' = ["half", "standard"]
+}
+
+#assignment 3
+
+#fill in the words
+
+adj1 = raw_input("noisy:")
+adj2 = raw_input("lonely:")
+noun1 = raw_input("speaker:")
+noun2 = raw_input("shark:")
+noun3 = raw_input("golf clubs:")
+game = raw_input("Monopoly:")
+noun4 = raw_input("towels:")
+gerund1 = raw_input("swimming:")
+gerund2 = raw_input("cleaning:")
+noun5 = raw_input("wines:")
+gerund3 = raw_input("yawning:")
+noun6 = raw_input("popcorn:")
+plant = raw_input("palm tree:")
+body = raw_input("hand:")
+place = raw_input("cocktail bar:")
+gerund4 = raw_input("sleeping:")
+adj3 =raw_input("cloudy:")
+number = raw_input("123:")
+noun7 = raw_input("streets:")
+
+#story 
+# From VACATION FUN MAD LIBS® • Copyright © 1988 by Price Stern Sloan, a division of Penguin Putnam Books for Young Readers, New York.
+story = "A vacation is when you take a trip to some " + adj1 + "place with your " + adj2 + "family. " + "Usually you go to some place that is near a/an " + noun1 + "or up on a/an" + noun2 + "." + "A good vacation place is one where you can ride " + noun3 + "or play " + game + "or go hunting for " + noun4 + "." + "I like to spend my time " + gerund1 + "or " + gerund2 + "." + "When parents go on a vacation, they spend their time eating three "+ noun5 + "a day, and fathers play golf, and mothers sit around " + gerund3 + "." + "Last summer, my little brother fell in a/an " + noun6 + "and got poison " + plant + "all over his " + body + "." + "My family is going to go to (the) " + place + ", " + "and I will practice " + gerund4 + "." + "Parents need vacations more than kids because parents are always very " + adj3 + "and because they have to work " + number + "hours every day all year making enough " + noun7 + "to pay for the vacation."
+print (story)
